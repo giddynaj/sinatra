d531916540ddc892c0b8e9a36a1386b68ace6a1d
giddynaj <eberjan@gmail.com>
2014-09-07 20:28:35 -0400

    added bootstrap and angular

---
+++ public/js/rejourn-ctrl.js
@@ -0,0 +1,5 @@
+var rejornApp = angular.module('rejornApp', []);
+
+rejornApp.controller('rejornCtrl', ['$scope', function rejornApp($scope){
+                     $scope.username = 'Eberjan';
+}]);
