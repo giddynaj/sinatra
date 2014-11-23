a65bc29b67e98859eafafce57291a8dd0f26f290
puruggae <eberjan.purugganan@dealix.com>
2014-11-23 07:08:36 -0500

    Adding advanced tagging and google search

--- sinatra/public/js/tag-ctrl.js
+++ sinatra/public/js/tag-ctrl.js
@@ -1,8 +1,36 @@
 var tagCtrl= function($scope, $http){
   $scope.add_tag = '';
+  $scope.keys = [];
   $scope.tags = [];
+  $scope.hashes = [];
   $scope.tag_results = 0;
   $scope.context = '';
+
+
+  $scope.set_things = function(query){
+     if (query.indexOf(":") > -1) { 
+     $scope.set_hash(query);
+     $scope.get_hash($scope.context);
+     } else {
+     $scope.set_tags(query);
+     $scope.get_tags($scope.context);
+     }
+  }
+
+  $scope.get_things = function(context){
+     $scope.get_tags($scope.context);
+     $scope.get_hash($scope.context);
+  }
+ 
+  $scope.get_keys= function(){
+       var responsePromise = $http.get('http://ec2-54-213-216-43.us-west-2.compute.amazonaws.com:8080/tags',{ params: {'keys' : 'True'}})
+       responsePromise.success(function(dataFromServer, status, headers, config){
+        $scope.keys = dataFromServer['kmembers']
+       });
+       responsePromise.error(function(data, status, headers, config){
+         alert("Submitting form failed!");
+       });
+  };
 
   $scope.set_tags= function(query){
        var dataObject = { 'context' : $scope.context, 'tag' : query }
@@ -10,6 +38,17 @@ var tagCtrl= function($scope, $http){
        responsePromise.success(function(dataFromServer, status, headers, config){
         $scope.tag_results = dataFromServer['results']
         $scope.tags = dataFromServer['members']
+       });
+       responsePromise.error(function(data, status, headers, config){
+         alert("Submitting form failed!");
+       });
+  };
+  $scope.set_hash= function(query){
+       var dataObject = { 'context' : $scope.context, 'tag' : query.split(":")[0], 'value' : query.split(":")[1] }
+       var responsePromise = $http.post('http://ec2-54-213-216-43.us-west-2.compute.amazonaws.com:8080/tags',dataObject,{})
+       responsePromise.success(function(dataFromServer, status, headers, config){
+        $scope.tag_results = dataFromServer['results']
+        $scope.hashes = dataFromServer['hmembers']
        });
        responsePromise.error(function(data, status, headers, config){
          alert("Submitting form failed!");
@@ -38,6 +77,12 @@ var tagCtrl= function($scope, $http){
          alert("Submitting form failed!");
        });
   };
+
+  function init(){ 
+    $scope.get_keys();
+  }
+  init();
+
 };
                 
 
